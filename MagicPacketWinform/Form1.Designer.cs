e3ae477859432327ac709a13b685bdba8c385408
tarzanzito <tarzanzito@gmail.com>
2026-05-06 21:20:37 +0100

    Load event correction

--- MagicPacketWinform/Form1.Designer.cs
+++ MagicPacketWinform/Form1.Designer.cs
@@ -296,6 +296,7 @@ namespace MagicPacketWinform
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Send Wake On Lan";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
